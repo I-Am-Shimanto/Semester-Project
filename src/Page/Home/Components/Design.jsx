7359f0929a87c0eaf2229aea8a60440f60259b0e
Shimanto Web <shimantowebdeveloper@gmail.com>
2025-01-04 23:15:46 +0600

    design done

---
+++ src/Page/Home/Components/Design.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Design = () => {
+  return (
+    <>
+      <section className='mt-[42px]'>
+        <div className="container">
+          <div className="row flex justify-between px-6">
+
+            <div className="single-card w-[394.67px] h-[429px] pt-[64px]">
+              <img className='mx-auto' src="images/design1.png" alt="design1" />
+              <h2 className='text-center pt-[42px] pb-[12px] text-[27px] font-semibold font-epilogue text-brand-color leading-[42px]'>Product Design</h2>
+              <p className='w-[310.67px] mx-auto text-[17px] font-normal font-epilogue text-brand-color leading-[27px] text-center'>This is a template Figma file, turned into code using Anima. Learn more at AnimaApp.com</p>
+            </div>
+
+            <div className="single-card w-[394.67px] h-[429px] pt-[64px]">
+              <img className='mx-auto' src="images/design2.png" alt="design1" />
+              <h2 className='text-center pt-[42px] pb-[12px] text-[27px] font-semibold font-epilogue text-brand-color leading-[42px]'>Visual Design</h2>
+              <p className='w-[310.67px] mx-auto text-[17px] font-normal font-epilogue text-brand-color leading-[27px] text-center'>This is a template Figma file, turned into code using Anima. Learn more at AnimaApp.com</p>
+              </div>
+
+            <div className="single-card w-[394.67px] h-[429px] pt-[64px]">
+              <img className='mx-auto' src="images/design3.png" alt="design1" />
+              <h2 className='text-center pt-[42px] pb-[12px] text-[27px] font-semibold font-epilogue text-brand-color leading-[42px]'>Art Direction</h2>
+              <p className='w-[310.67px] mx-auto text-[17px] font-normal font-epilogue text-brand-color leading-[27px] text-center'>This is a template Figma file, turned into code using Anima. Learn more at AnimaApp.com</p>
+              </div>
+
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Design
--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Navbar from './Components/Navbar'
 import Banner from './Components/Banner'
 import Brands from './Components/Brands'
+import Design from './Components/Design'
 
 const Home = () => {
   return (
     <>
       <Navbar />
       <Banner />
-      <Brands/>
+      <Brands />
+      <Design/>
     </>
   )
 }
